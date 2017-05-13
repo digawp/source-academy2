45446142216d713a4518617c3647980f98aca8d9
Evan Sebastian <evanlhoini@gmail.com>
2017-05-13 20:10:15 +0800

    [frontend] Stubs some model types

--- frontend/modules/sa/core/types.ts
+++ frontend/modules/sa/core/types.ts
@@ -1,10 +1,71 @@
 import { Store, Reducer } from 'redux'
 
+// Domain Models
+export interface IUser {
+  id: number
+  role: "admin" | "staff" | "student"
+  firstName: string
+  lastName: string
+  profilePicture: string
+}
+
+export interface IAnnouncement {
+  id: number
+  poster: number
+  published: boolean
+  pinned: boolean
+  updatedAt: number
+  pinExpiry: number
+  title: string
+  content: string
+}
+
+export interface IAssessment {
+  id: number
+  type: "mission" | "sidequest" | "path"
+  order: string
+  published: boolean
+  title: string
+  description: string
+  coverPicture: string
+  openedAt: number
+  dueAt: number
+}
+
+export interface IStudent {
+  id: number
+  user: number
+  level: number
+  experiencePoint: number
+}
+
+export interface IAnswer {
+  id: number
+  question: number
+  student: number
+  code?: string
+  mcqChoice?: number
+}
+
+export interface IGrading {
+  id: number
+  status: "not_opened" | "attempting" | "submitted" | "manual"
+  assessment?: number
+  student: number
+  gradedBy: number
+  gradedAt: number
+  marksObtained?: number
+  experiencePoint?: number
+}
+
+// UI Types
+
+// Globals
 export interface AsyncStore extends Store<any> {
   asyncReducers: {[index: string]: Reducer<any>}
 }
-export interface IAppDelegate {
 
+export interface IAppDelegate {
   store: AsyncStore
 
   injectReducers: (
