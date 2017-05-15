aa0b30a8b0dbcde7fc8498f3ae2e1f70f25d294a
Evan Sebastian <evanlhoini@gmail.com>
2017-05-16 04:20:07 +0800

    [frontend] Fix code splitting problem

--- frontend/modules/sa/academy/loader.ts
+++ frontend/modules/sa/academy/loader.ts
@@ -1,18 +1,13 @@
-import { Store } from 'redux'
 import { BundleLoader } from 'sa/core/types'
 import { academyBundleLoaded } from 'sa/core/util'
-import { reducer as assessment } from './reducers/assessment'
-import { reducer as currentStudent } from './reducers/currentStudent'
-import { reducer as announcement } from './reducers/announcement'
-import academySaga from './sagas'
 
 const loader: BundleLoader = (app, bundleLoaded) => {
   require.ensure([], () => {
     const AcademyContainer = require('./containers/AcademyContainer').default
+    const academySaga = require('./sagas')
+    const reducers = require('./reducers')
 
     require("./styles/index.scss")
-
-    const reducers = { currentStudent, assessment, announcement }
 
     app.injectReducers(reducers)
     app.injectSaga('academy', academySaga)
@@ -23,11 +18,9 @@ const loader: BundleLoader = (app, bundleLoaded) => {
           const NewContainer = require('./containers/AcademyContainer').default
           bundleLoaded(NewContainer)
         });
-        ['inbox', 'assessment', 'currentStudent', 'announcement'].forEach(key => {
-          module.hot.accept(`./reducers/${key}`, () => {
-            const { reducer } = require(`./reducers/${key}`)
-            app.injectReducers({ [key]: reducer })
-          })
+        module.hot.accept(`./reducers/`, () => {
+          const reducers = require(`./reducers`).default
+          app.injectReducers(reducers)
         })
       }
     }
---
+++ frontend/modules/sa/academy/reducers/index.ts
@@ -0,0 +1,9 @@
+import { reducer as assessment } from './assessment'
+import { reducer as currentStudent } from './currentStudent'
+import { reducer as announcement } from './announcement'
+
+export default {
+  assessment,
+  currentStudent,
+  announcement
+}
