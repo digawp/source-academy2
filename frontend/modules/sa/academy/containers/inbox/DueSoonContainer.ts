9a0c8cb8a99f43ff895c798f04f3aabfdba6843d
Evan Sebastian <evanlhoini@gmail.com>
2017-05-17 14:47:05 +0800

    [frontend] Use reselect in DueSoon

--- frontend/modules/sa/academy/containers/inbox/DueSoonContainer.ts
+++ frontend/modules/sa/academy/containers/inbox/DueSoonContainer.ts
@@ -1,33 +1,33 @@
 import * as moment from 'moment'
+import { values } from 'lodash'
+import { createSelector } from 'reselect'
 import { RouteComponentProps } from 'react-router'
 import { connect } from 'react-redux'
 import { IAssessment } from 'sa/core/types'
 import { State } from '../../types'
 import DueSoon from '../../components/inbox/DueSoon'
 
-const selectDueSoon = (assessments: { [id: number]: IAssessment}, type: string) => {
-  const values: IAssessment[] = []
+const getAssessments = (state: State) => state.assessment
 
-  for (let key of Object.keys(assessments)) {
-    values.push(assessments[parseInt(key, 10)])
-  }
-
-  return values.filter(
-    (a: IAssessment) => {
-      if (a.type !== type) {
-        return false
-      } else {
-        const due = moment(a.dueAt)
-        const now = moment()
-        return due.subtract(7, 'days').startOf('day').isBefore(now)
-      }
-    })
-}
+const selectByType = (type: string) =>
+  createSelector(
+    getAssessments,
+    (assessments) =>
+      values(assessments).filter((a) => {
+        if (a.type !== type) {
+          return false
+        } else {
+          const due = moment(a.dueAt)
+          const now = moment()
+          return due.subtract(7, 'days').startOf('day').isBefore(now)
+        }
+      })
+  )
 
 const mapStateToProps = (state: State, ownProps: RouteComponentProps<any>) => ({
-  sidequests: selectDueSoon(state.assessment, 'sidequest'),
-  paths: selectDueSoon(state.assessment, 'path'),
-  missions: selectDueSoon(state.assessment, 'mission')
+  sidequests: selectByType('sidequest')(state),
+  paths: selectByType('path')(state),
+  missions: selectByType('mission')(state)
 })
 
 export default connect(mapStateToProps)(DueSoon)
