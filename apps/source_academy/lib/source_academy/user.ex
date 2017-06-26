61a4e581c54b8003a73ed5676f2672222782efb8
Diga Widyaprana <digawp@gmail.com>
2017-06-26 17:56:48 +0800

    Add User and Authentication API to SourceAcademy

---
+++ apps/source_academy/lib/source_academy/user.ex
@@ -0,0 +1,48 @@
+defmodule SourceAcademy.User do
+  @moduledoc """
+    The User entity contains basic user data such as name and e-mail.
+    A user can have many authorisations from different providers.
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias SourceAcademy.Repo
+
+  @type t :: %__MODULE__{}
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :is_admin, :boolean
+
+    has_many :authorizations, SourceAcademy.Authorization
+
+    timestamps()
+  end
+
+  @required_fields ~w(name email)a
+  @optional_fields ~w()a
+
+  def build(params) do
+    changeset(%SourceAcademy.User{}, params)
+  end
+
+  def registration_changeset(user, params \\ :empty) do
+    user
+    |> cast(params, ~w(email name)a)
+    |> validate_required(@required_fields)
+    |> validate_format(:email, ~r/.*@.*/)
+  end
+
+  def changeset(user, params \\ :empty) do
+    user
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> validate_format(:email, ~r/.*@.*/)
+  end
+
+  def make_admin!(user) do
+    user
+    |> cast(%{is_admin: true}, ~w(is_admin)a)
+    |> Repo.update!
+  end
+end
