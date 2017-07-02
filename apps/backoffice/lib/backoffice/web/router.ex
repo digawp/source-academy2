b9a81b18363d8a15e374361c3a5096f7bff4cca6
Diga Widyaprana <digawp@gmail.com>
2017-07-02 19:41:43 +0800

    Add auth stuff to backoffice

--- apps/backoffice/lib/backoffice/web/router.ex
+++ apps/backoffice/lib/backoffice/web/router.ex
@@ -9,16 +9,30 @@ defmodule Backoffice.Web.Router do
     plug :put_secure_browser_headers
   end
 
+  pipeline :browser_auth do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", Backoffice.Web do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:browser, :browser_auth]
 
     get "/", PageController, :index
+    get "/login", AuthController, :login
   end
 
+  scope "/auth", Backoffice.Web do
+    pipe_through [:browser, :browser_auth]
+
+    get "/logout", AuthController, :logout
+    get "/:identity", AuthController, :request
+    get "/:identity/callback", AuthController, :callback
+    post "/:identity/callback", AuthController, :callback
+  end
   # Other scopes may use custom stacks.
   # scope "/api", Backoffice.Web do
   #   pipe_through :api
