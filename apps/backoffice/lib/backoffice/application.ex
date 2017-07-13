b2df324636ecae7958c10b9485bf7148e4320681
Evan Sebastian <evanlhoini@gmail.com>
2017-07-13 17:39:43 +0800

    Remove Repo from Backoffice

--- apps/backoffice/lib/backoffice/application.ex
+++ apps/backoffice/lib/backoffice/application.ex
@@ -8,8 +8,6 @@ defmodule Backoffice.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
-      supervisor(Backoffice.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Backoffice.Web.Endpoint, []),
       # Start your own worker by calling: Backoffice.Worker.start_link(arg1, arg2, arg3)
