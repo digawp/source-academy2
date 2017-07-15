ddf913316110d78450943938eff1611e0bdecd6d
Evan Sebastian <evanlhoini@gmail.com>
2017-07-15 15:22:44 +0800

    Improve Show User UI

--- apps/backoffice/lib/backoffice/web/controllers/user_controller.ex
+++ apps/backoffice/lib/backoffice/web/controllers/user_controller.ex
@@ -1,38 +1,22 @@
 defmodule Backoffice.Web.UserController do
-
   use Backoffice.Web, :controller
-  use Guardian.Phoenix.Controller
+
   alias SourceAcademy.Repo
   alias SourceAcademy.User
 
   plug Guardian.Plug.EnsureAuthenticated,
     [handler: __MODULE__, typ: "access"] when action in [:edit, :index]
 
-  def index(conn, _params, current_user, _claims) do
-    users = Repo.all(User)
-    render(conn, "index.html", current_user: current_user, users: users)
-  end
-
-  def new(conn, _params, _current_user, _claims) do
-    render(conn, "new.html")
-  end
-
-  def show(conn, _params, current_user, _claims) do
-    render(conn, "show.html", current_user: current_user)
-  end
-
-  def unauthenticated(conn, _params) do
-    conn
-    |> put_flash(:error, "Authentication required")
-    |> redirect(to: auth_path(conn, :login))
-  end
-
-  def edit(conn, %{"id" => id}, current_user, _claims) do
+  def show(conn, %{"id" => id}) do
     user = Repo.get!(User, id)
-    changeset = User.changeset(current_user)
-    render(conn, "edit.html", user: current_user, changeset: changeset)
+    render(conn, "show.html", user: user)
   end
 
+  def edit(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user)
+    render(conn, "edit.html", user: user, changeset: changeset)
+  end
   # def update(conn, %{"id" => id, "user" => user_params}, current_user, _claims) do
   #   user = Repo.get!(User, id)
 
